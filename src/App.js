129d3a1857024cbbc03aa783e703e3b18855160c
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-11-20 22:03:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import TopHeader from './components/header/TopHeader';
 
 function App() {

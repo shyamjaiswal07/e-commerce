98113a5e9035dd3c60a4db96dee6d9ae2f9607b8
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-11-20 23:40:16 +0530

    Update MainHeader.jsx

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -8,7 +8,7 @@ const MainHeader = () => {
       <TopHeader />
       <>
        <nav class="navbar navbar-expand-lg ">
-  <div class="container-fluid">
+  <div class="container">
     <a class="navbar-brand" href="#">Hello</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>

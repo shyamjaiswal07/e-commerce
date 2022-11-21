efd07f59aad37343be69eb4976bbc206128e915c
Shyam Jaiswal <106074037+shyamjaiswal07@users.noreply.github.com>
2022-11-21 21:19:55 +0530

    logo

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -10,9 +10,9 @@ const MainHeader = () => {
     <>
       <TopHeader />
       <>
-       <nav class="navbar navbar-expand-lg ">
+       <nav class="navbar navbar-expand-lg">
   <div class="container">
-    <a class="navbar-brand" href="#"><img className="h-25 w-50" src="./Images/logo.png" alt="" /></a>
+    <a class="navbar-brand" ><img className="h-25 w-50" src="./Images/logo.png" alt="" /></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
